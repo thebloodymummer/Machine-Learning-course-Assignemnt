c9510ddfe70e787ed98036c7ab3b26d2bb2fb45d
the_bloody_mummer <obriennano@gmail.com>
2018-05-28 14:07:36 +0100

    final commit

--- Assignemnt.R
+++ Assignemnt.R
@@ -13,9 +13,19 @@ traindest <- "./rawData/train.csv"
 testdest <- "./rawData/test.csv"
 download.file(train, destfile = traindest)
 download.file(test, destfile = testdest)
-trainData <- read.csv("./rawData/train.csv")
-testData <- read.csv("./rawData/test.csv")
-  
+trainData <- read.csv("./rawData/train.csv", na.strings=c("NA","#DIV/0!", ""))
+testData <- read.csv("./rawData/test.csv", na.strings=c("NA","#DIV/0!", ""))
+
+trainData <- trainData[,8:length(colnames(trainData))]
+testData <- testData[,8:length(colnames(testData))]
+
+trainData <- trainData[, colSums(is.na(trainData)) == 0] 
+testData <- testData[, colSums(is.na(testData)) == 0]
+
+nzv <- nearZeroVar(trainData,saveMetrics=TRUE)
+zero.var.ind <- sum(nzv$nzv)
+if ((zero.var.ind>0)) { trainData <- trainData[,nzv$nzv==FALSE]}
+
 inTrain <- createDataPartition(y=trainData$classe, p=0.6, list=FALSE)
 trainData2 <- trainData[inTrain, ] 
 validation <- trainData[-inTrain, ]
@@ -25,26 +35,15 @@ ggplot(trainData2,
        aes(x = classe)) + geom_bar() + labs(main = "Plot of levels of variable classes",
                                             x = "Classe", 
                                             y = "Frequency")
-##Classification Tree
+
 classModel <- rpart(classe ~ ., data=trainData2, method="class")
-
 classPredication <- predict(classModel, validation, type = "class")
-
-fancyRpartPlot(classModel$finalModel)
-
+fancyRpartPlot(classModel)
 confusionMatrix(classPredication, validation$classe)
 
-##Random Forest
 forestModel <- randomForest(classe ~. , data=trainData2, method="class")
-
-# Predicting:
-forestPrediction <- predict(ForestModel, validation, type = "class")
-
-# Test results on TestTrainingSet data set:
+forestPrediction <- predict(forestModel, validation, type = "class")
 confusionMatrix(forestPrediction, validation$classe)
 
-#Final Prediction
+
 predict(forestModel, testData, type="class")
-
-
-
